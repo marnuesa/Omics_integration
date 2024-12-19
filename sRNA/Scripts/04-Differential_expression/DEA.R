8f8cf3d15d57d6125e9a1536ba926522071f7254
marnuesa <87702176+marnuesa@users.noreply.github.com>
2024-12-19 09:58:14 +0100

    Update DEA.R
    eliminate normalization part

--- sRNA/Scripts/04-Differential_expression/DEA.R
+++ sRNA/Scripts/04-Differential_expression/DEA.R
@@ -139,7 +139,6 @@ path_graph <- args$graphs
 # Create output paths
 path_raw_out <- paste(path_out, '01-DEA_raw', specie, project, sep = '/')
 path_sig_out <- paste(path_out, '02-DEA_sig', specie, project, sep = '/')
-path_normalize <- paste(path_out, '03-Table_normalize', specie, project, sep = '/')
 path_out_ea <- paste(path_graph, '01-PCA_graphs', sep = '/')
 path_out_vp <- paste(path_graph, '02-Volcano_plots', sep = '/')
 
@@ -171,12 +170,6 @@ for (time in unique(metadata$Time)) {
   # Pre-filtering; It filters sequences that are less than 5 times in 5 samples
   keep <- rowSums(countdata_subproject > 5) >= 5
   dds_matrix <- dds_matrix[keep,]
-
-  # Perform the median of ratios method of normalization
-  dds_micro_norm <- estimateSizeFactors(dds_matrix)
-
-  # Get the normalized matrix
-  micro_normalized_counts <- counts(dds_micro_norm, normalized=TRUE)
   
   # Exploratory analysis and visualization (variance stabilizing transformation)
   vsd_dds <- vst(dds_matrix, blind = FALSE)
@@ -261,16 +254,5 @@ for (time in unique(metadata$Time)) {
       ggsave(paste0(path_out_vp,"/",stress,"_T",time,".png"), plot = p, width = 8, height = 6, dpi = 300)
     }
   }
-  # Filter normalize df
-  DE_sRNA_list <- unique(DE_sRNA_list)
-  micro_normalized_counts_filter <- micro_normalized_counts[rownames(micro_normalized_counts) %in% DE_sRNA_list, ]
-  micro_normalized_counts_filter_named <- cbind(row.name = rownames(micro_normalized_counts), micro_normalized_counts_filter)
-
-  # Save normalize counts table
-  write.table(micro_normalized_counts_filter_named, 
-            file=paste0(path_normalize, "/Table_normalize_counts_T", time, ".tsv"),
-            sep="\t", 
-            row.names=FALSE, 
-            col.names=TRUE,  
-            quote=FALSE) 
+  write.table(DE_sRNA_list, file = paste0(path_out,"DE_seq.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 }
