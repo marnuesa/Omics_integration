04fbef740266e66628b9bb4152e7027266a19154
marnuesa <87702176+marnuesa@users.noreply.github.com>
2025-02-21 16:16:57 +0100

    Update DEA_transcripts.R

--- RNA_seq/Scripts/03-DEA/DEA_transcripts.R
+++ RNA_seq/Scripts/03-DEA/DEA_transcripts.R
@@ -167,11 +167,11 @@ colnames(annotation_file) <- c('Gene','Description')
 ############################# BATCH1 ####################################
 # Definir colores fijos para cada tipo de condición
 color_dict <- c(
-  "cold" = "#f8766d",        
-  "control" = "#a3a500",     
-  "drought" = "#00bf7d",     
-  "shortday" = "#e76bf3",    
-  "monosporascus" = "#00b0f6" 
+  "cold" = "#1E90FF",        
+  "control" = "#CDC9C9",     
+  "drought" = "#EECFA1",     
+  "shortday" = "#0F0F0F",    
+  "monosporascus" = "#98FB98" 
 )
 
 metadata_batch1 <- metadata[metadata$Batch == 1,]
@@ -214,7 +214,8 @@ for (time in unique(metadata_batch1$Time)) {
 
   ## Create and save Principal Component Analysis
   PCA <- plotPCA(vsd_dds, intgroup = c("Group"))+
-  scale_color_manual(values = palette_colors)
+  scale_color_manual(values = palette_colors) +
+  theme_classic()
   ggsave(paste0(path_out_ea,"/PCA_time",time,".png"), plot = PCA, width = 8, height = 6, dpi = 300)
   
   # Differential expression analysis
@@ -345,7 +346,8 @@ for (time in unique(metadata_batch2$Time)) {
   
   ## Create and save Principal Component Analysis
   PCA <- plotPCA(vsd_dds, intgroup = c("Group")) + 
-  scale_color_manual(values = c("cold_3" = "#f8766d", "control_3" = "#a3a500"))
+  scale_color_manual(values = c("cold_3" = "#f8766d", "control_3" = "#a3a500")) +
+  theme_classic()
   ggsave(paste0(path_out_ea,"/PCA_time",time,"batch2.png"), plot = PCA, width = 8, height = 6, dpi = 300)
   
   # Differential expression analysis
