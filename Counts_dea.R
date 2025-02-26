0ed9112b4a420c0c6e17862671eb90ac3100001f
Marta Nunez Salvador nuezsal@alumni.uv.es <nuezsal@master.data.cpd>
2025-02-26 10:16:07 +0100

    summary DEAa

---
+++ Counts_dea.R
@@ -0,0 +1,158 @@
+# ==========================================================
+#                    Counts_dea.R
+# ==========================================================
+# Description:
+# ----------------------------------------------------------
+# This script processes omics data from three different sources:
+# 1. Methylome (DMRs)
+# 2. MicroRNA
+# 3. RNA-seq
+# 
+# It calculates the number of differentially expressed or regulated elements
+# for each omic type and organizes the data across various time points and 
+# stress conditions.
+# 
+# 
+# Author: Marta Núñez Salvador
+# Date: 22/09/2024
+# Version: 1.1
+################################################################################
+
+suppressMessages(library(argparse))
+suppressMessages(library(dplyr))
+
+################################## FUNCTIONS ###################################
+# Function to parse command-line arguments
+get_arguments <- function() {
+  parser <- ArgumentParser(prog = 'Conteos_dea.R',
+                           description = 'Processes omics data and generates summary tables.')
+  
+  required <- parser$add_argument_group('required arguments')
+  
+  required$add_argument('-m', '--methylome_path', type = 'character',
+                        help = 'Path to the methylome data directory.', required = TRUE)
+  required$add_argument('-mi', '--microrna_path', type = 'character',
+                        help = 'Path to the microRNA data directory.', required = TRUE)
+  required$add_argument('-r', '--rnaseq_path', type = 'character',
+                        help = 'Path to the RNA-seq data directory.', required = TRUE)
+  required$add_argument('-o', '--output_path', type = 'character',
+                        help = 'Path to the output directory.', required = TRUE)
+  
+  args <- parser$parse_args()
+  return(args)
+}
+
+# Save output arguments in variables
+output_path <- args$output_path
+
+# Create output directory if it does not exist
+dir.create(output_path, recursive = TRUE, showWarnings = FALSE)
+
+#################### Methylome ####################
+# Define the path for methylome data
+path_methylome <- args$methylome_path
+
+# List all the methylome TSV files
+files_methylome <- list.files(path_methylome, pattern = ".tsv", full.names = TRUE)
+
+# Initialize an empty dataframe for methylome
+df_methylome <- data.frame()
+for(file in files_methylome) {
+  # Extract the filename from the full file path
+  file_name <- basename(file)
+  
+  # Split the filename using '.' and '_' as delimiters, and extract the stress condition
+  stress <- strsplit(file_name, "[._]")[[1]][3]
+  
+  # Read the TSV file into a data frame
+  table <- read.table(file, header = TRUE, sep = "\t")
+  
+  # Add a new column called 'Stress' with the extracted value
+  table$Stress <- stress
+  
+  # Combine the current file's data with the main methylome data frame
+  df_methylome <- rbind(df_methylome, table)
+}
+
+# Convert the Time column to a factor if it's not already
+df_methylome$Time <- as.factor(df_methylome$Time)
+
+#################### MicroRNA ####################
+# Define the path for microRNA data
+path_microrna <- args$microrna_path
+
+# List all microRNA files
+files_microrna <- list.files(path_microrna, full.names = TRUE)
+
+# Initialize an empty list for microRNA data
+df_microrna <- list()
+for (file in files_microrna){
+  # Extract the filename from the full file path
+  file_name <- basename(file)
+  
+  # Split the filename using '.' and '_' as delimiters, and extract stress and time
+  stress <- strsplit(file_name, "[._]")[[1]][1]
+  time <- strsplit(file_name,"[._]")[[1]][2]
+  
+  # Read the CSV file into a data frame
+  table <- read.table(file, sep = ",", header = TRUE)
+  
+  # Add a column for differential expression (up or down-regulated)
+  table$DE_type <- ifelse(table$Shrunkenlog2FoldChange < 0, "Infra-expresado", "Sobre-expresado")
+  
+  # Select only relevant columns and add Stress and Time columns
+  table <- table[, c("seq", "DE_type")]
+  table$Stress <- stress
+  table$Time <- time
+  
+  # Combine the current file's data with the main microRNA data frame
+  df_microrna <- rbind(df_microrna, table)
+}
+
+# Group by Time, Stress, and DE_type to count occurrences
+df_microrna_counts <- df_microrna %>%
+  group_by(Time, Stress, DE_type) %>%
+  summarise(Count = n(), .groups = 'drop')
+
+#################### RNA-seq ####################
+# Define the path for RNA-seq data
+path_rnaseq <- args$rnaseq_path
+
+# List all RNA-seq files
+files_rnaseq <- list.files(path_rnaseq, full.names = TRUE)
+
+# Initialize an empty list for RNA-seq data
+df_rnaseq <- list()
+for (file in files_rnaseq){
+  # Extract the filename from the full file path
+  file_name <- basename(file)
+  
+  # Split the filename using '.' and '_' as delimiters, and extract stress and time
+  stress <- strsplit(file_name, "[._]")[[1]][1]
+  time <- strsplit(file_name,"[._]")[[1]][2]
+  
+  # Read the CSV file into a data frame
+  table <- read.table(file, sep = ",", header = TRUE)
+  if (nrow(table) != 0) {
+    # Add a column for differential expression (up or down-regulated)
+    table$DE_type <- ifelse(table$Shrunkenlog2FoldChange < 0, "Infra-expresado", "Sobre-expresado")
+    
+    # Select only relevant columns and add Stress and Time columns
+    table <- table[, c("seq", "DE_type")]
+    table$Stress <- stress
+    table$Time <- time
+    
+    # Combine the current file's data with the main RNA-seq data frame
+    df_rnaseq <- rbind(df_rnaseq, table)
+  }
+}
+
+# Group by Time, Stress, and DE_type to count occurrences
+df_rnaseq_counts <- df_rnaseq %>%
+  group_by(Time, Stress, DE_type) %>%
+  summarise(Count = n(), .groups = 'drop')
+
+
+write.table(df_methylome,file = paste0(path_out,"/summary_methylome.tsv"),sep = "\t",col.names = TRUE,row.names = FALSE,quote = FALSE)
+write.table(df_microrna_counts,file = paste0(path_out,"/summary_microRNA.tsv"),sep = "\t",col.names = TRUE,row.names = FALSE,quote = FALSE)
+write.table(df_rnaseq_counts,file = paste0(path_out,"/summary_transcripts.tsv"),sep = "\t",col.names = TRUE,row.names = FALSE,quote = FALSE)
---
+++ Counts_dea_submit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=Counts_dea   # Job name to show with squeue
+#SBATCH --output=Counts_dea_%j.out  # Output file
+#SBATCH --ntasks=1                   # Maximum number of cores to use
+#SBATCH --time=00-07:00:00           # Time limit to execute the job
+#SBATCH --mem-per-cpu=50G            # Required Memory per core
+#SBATCH --cpus-per-task=2            # CPUs assigned per task.
+#SBATCH --qos=short                  # QoS: short, medium, long, long-mem
+
+#******************************************************************************
+#
+# Counts_dea_submit.sh
+#
+# This script executes the Counts_dea.R script to process omics data
+# (methylome, microRNA, and RNA-seq) and generate differential expression counts.
+#
+# Author: Marta Núñez Salvador
+# Date: 22/09/2024
+# Version: 1.0
+#
+#******************************************************************************
+
+# Load necessary modules
+module load anaconda
+source activate group_sRNA
+
+# Paths
+methylome_path="/home/nuezsal/Omics_integration/Methylome/Results/04-DMRs_plots"
+microRNA_path="/home/nuezsal/Omics_integration/sRNA/Results/06-miRNAs_grouped_by_family/Group_miRNAs_sig/cume/Omics_project/01-DEA_results_annot"
+rnaseq_path="/home/nuezsal/Omics_integration/RNA_seq/Results/03-DEA_TH/02-DEA_sig/cume/Omics_project"
+output_path="/home/marnuesa/Documentos/Omics_integration/Summary"
+
+# Execution
+srun -N1 -n1 -c$SLURM_CPUS_PER_TASK --quiet --exclusive Rscript Counts_dea.R \
+  --methylome_path $methylome_path \
+  --microRNA_path $microRNA_path \
+  --rnaseq_path $rnaseq_path \
+  --output_path $output_path
