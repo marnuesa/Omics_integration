421acdd64c26e6c32cb99fa01bd278522d142fb7
marnuesa <87702176+marnuesa@users.noreply.github.com>
2025-02-27 15:56:31 +0100

    Update DEA_transcripts_submit.sh

--- RNA_seq/Scripts/03-DEA/DEA_transcripts_submit.sh
+++ RNA_seq/Scripts/03-DEA/DEA_transcripts_submit.sh
@@ -32,7 +32,7 @@ path_metadata=/home/nuezsal/Omics_integration/RNA_seq/Additional_info
 path_dea=/home/nuezsal/Omics_integration/RNA_seq/Results/03-DEA_TH
 path_graph=/home/nuezsal/Omics_integration/RNA_seq/Results/DESeq_graphs_TH
 alpha=0.05
-TH=0.858
+TH=0.585
 
 
 # Execution 
