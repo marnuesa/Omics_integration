d649e2ffa3e8a4483141ae39670b9f571e360d83
marnuesa <87702176+marnuesa@users.noreply.github.com>
2025-04-17 12:42:24 +0200

    Update methylome_submit.sh

--- Integration_MORE/Scripts/03-Methylome/methylome_submit.sh
+++ Integration_MORE/Scripts/03-Methylome/methylome_submit.sh
@@ -13,7 +13,6 @@
 #   methylome_submit.sh
 #
 #   This program generate normalize count tables of methylome
-#   and execute a integration with MLR model of MORE
 #
 #   Author: Marta Nuñez Salvador
 #   Date: 18/12/2024
